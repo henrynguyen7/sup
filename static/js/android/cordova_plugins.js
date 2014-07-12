d10d5dcc1160096744a284c51283f50c0ed8bbe1
Henry Nguyen <henry@dxconcept.com>
2014-07-11 22:58:15 -0700

    Added app preferences plugin (https://github.com/apla/me.apla.cordova.app-preferences).

--- static/js/android/cordova_plugins.js
+++ static/js/android/cordova_plugins.js
@@ -1,17 +1,24 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "PushNotification.js",
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
         "clobbers": [
             "PushNotification"
         ]
     },
     {
-        "file": "device.js",
+        "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
             "device"
+        ]
+    },
+    {
+        "file": "plugins/me.apla.cordova.app-preferences/www/apppreferences.js",
+        "id": "me.apla.cordova.app-preferences.apppreferences",
+        "clobbers": [
+            "plugins.appPreferences"
         ]
     }
 ]
